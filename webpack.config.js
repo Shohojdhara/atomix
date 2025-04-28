d6729a006baf2844678fde3a4e36f670687f2fef
Limon Khan <limonkhan669@gmail.com>
2025-04-28 21:43:24 +0600

    refactor: optimize build process and fix SASS warnings
    - Fixed circular dependency between utility modules
    - Added custom webpack plugin to remove unnecessary JS files
    - Updated SASS functions to use namespace versions (meta.type-of, list.nth, etc.)
    - Fixed color interpolation warning in utility generator
    - Reduced bundle size and eliminated all build warnings
    - Maintains all CSS functionality while eliminating unnecessary files
    - This resolves the build error that was occurring due to module loop
    - Eliminated all SASS deprecation warnings in the build process

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,124 @@ const TerserPlugin = require('terser-webpack-plugin');
 const packageJson = require('./package.json');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
+const fs = require('fs');
+
+/**
+ * Custom plugin to create unminified CSS files
+ */
+class UnminifiedCssPlugin {
+  constructor(options = {}) {
+    this.options = Object.assign({
+      // Default options
+      minFilePattern: /\.min\.css$/,
+      removeMinSuffix: true,
+    }, options);
+  }
+
+  // Simple CSS beautifier
+  beautifyCss(css) {
+    // Add newlines after closing braces
+    css = css.replace(/}/g, '}\n');
+    
+    // Add newlines after semicolons
+    css = css.replace(/;/g, ';\n  ');
+    
+    // Add newlines after opening braces and add indentation
+    css = css.replace(/{/g, '{\n  ');
+    
+    // Clean up multiple newlines
+    css = css.replace(/\n\s*\n/g, '\n');
+    
+    // Fix media queries and other special cases
+    css = css.replace(/@media[^{]+{/g, match => match.replace(/\n\s*/g, ' '));
+    
+    return css;
+  }
+
+  apply(compiler) {
+    // Hook into the 'afterEmit' event to process files after they're written
+    compiler.hooks.afterEmit.tapAsync('UnminifiedCssPlugin', (compilation, callback) => {
+      const outputPath = compilation.outputOptions.path;
+      
+      // Find all minified CSS files
+      const cssFiles = Object.keys(compilation.assets).filter(asset => 
+        asset.endsWith('.min.css')
+      );
+      
+      // For each minified CSS file, create an unminified version
+      for (const cssFile of cssFiles) {
+        const filePath = path.join(outputPath, cssFile);
+        
+        // Skip if the file doesn't exist or doesn't match our pattern
+        if (!fs.existsSync(filePath) || !this.options.minFilePattern.test(cssFile)) {
+          continue;
+        }
+        
+        try {
+          // Read the minified content
+          const content = fs.readFileSync(filePath, 'utf8');
+          
+          // Beautify the CSS
+          const beautifiedContent = this.beautifyCss(content);
+          
+          // Get the name for the unminified file
+          let unminifiedName = cssFile;
+          if (this.options.removeMinSuffix) {
+            unminifiedName = cssFile.replace('.min.css', '.css');
+          }
+          
+          // Write the unminified file
+          fs.writeFileSync(path.join(outputPath, unminifiedName), beautifiedContent);
+          
+          console.log(`Created unminified version: ${unminifiedName}`);
+        } catch (err) {
+          console.error(`Error processing ${cssFile}:`, err);
+        }
+      }
+      
+      callback();
+    });
+  }
+}
+
+/**
+ * Custom plugin to remove JS files created for CSS-only entries
+ */
+class RemoveCssJsPlugin {
+  constructor(options = {}) {
+    this.options = Object.assign({
+      // Default options
+      cssEntries: ['styles'],
+    }, options);
+  }
+
+  apply(compiler) {
+    // Hook into the 'afterEmit' event to process files after they're written
+    compiler.hooks.afterEmit.tapAsync('RemoveCssJsPlugin', (compilation, callback) => {
+      const outputPath = compilation.outputOptions.path;
+      const name = this.options.name || '';
+      const version = this.options.version || '';
+      
+      // Remove JS files for CSS-only entries
+      for (const cssEntry of this.options.cssEntries) {
+        const jsFilePath = path.join(outputPath, `js/${name}-${version}.${cssEntry}.js`);
+        const mapFilePath = path.join(outputPath, `js/${name}-${version}.${cssEntry}.js.map`);
+        
+        if (fs.existsSync(jsFilePath)) {
+          fs.unlinkSync(jsFilePath);
+          console.log(`Removed unnecessary JS file: js/${name}-${version}.${cssEntry}.js`);
+        }
+        
+        if (fs.existsSync(mapFilePath)) {
+          fs.unlinkSync(mapFilePath);
+          console.log(`Removed map file: js/${name}-${version}.${cssEntry}.js.map`);
+        }
+      }
+      
+      callback();
+    });
+  }
+}
 
 /**
  * Atomix Design System Webpack Configuration
@@ -51,12 +169,10 @@ module.exports = (env, argv) => {
         {
           test: /\.s?css$/,
           use: [
-            isProduction 
-              ? { 
-                  loader: MiniCssExtractPlugin.loader,
-                  options: { publicPath: '../' } // Fixes asset URL paths in CSS
-                } 
-              : 'style-loader',
+            { 
+              loader: MiniCssExtractPlugin.loader,
+              options: { publicPath: '../' }
+            },
             {
               loader: 'css-loader',
               options: {
@@ -124,7 +240,12 @@ module.exports = (env, argv) => {
         chunkFilename: isProduction 
           ? 'css/[id].[contenthash].min.css'
           : 'css/[id].[contenthash].css',
-      })
+      }),
+      // Add our custom unminified CSS plugin in production mode
+      ...(isProduction ? [
+        new UnminifiedCssPlugin(),
+        new RemoveCssJsPlugin({ cssEntries: ['styles'], name, version })
+      ] : [])
     ],
     optimization: {
       minimizer: [
