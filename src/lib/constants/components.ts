184e306a723808a7419cbcab4200bbe4192fac0c
liimonx <limonkhan669@gmail.com>
2025-05-11 04:36:44 +0600

    feat: add Breadcrumb components

--- src/lib/constants/components.ts
+++ src/lib/constants/components.ts
@@ -75,6 +75,26 @@ export const BADGE = {
 };
 
 /**
+ * Breadcrumb-specific constants
+ */
+export const BREADCRUMB = {
+  SELECTORS: {
+    BREADCRUMB: '.c-breadcrumb',
+    ITEM: '.c-breadcrumb__item',
+    LINK: '.c-breadcrumb__link'
+  },
+  CLASSES: {
+    BASE: 'c-breadcrumb',
+    ITEM: 'c-breadcrumb__item',
+    LINK: 'c-breadcrumb__link',
+    ACTIVE: 'is-active'
+  },
+  DEFAULTS: {
+    DIVIDER: '›'
+  }
+};
+
+/**
  * Countdown-specific constants
  */
 export const COUNTDOWN = {
