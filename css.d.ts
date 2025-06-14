da3e420531c7d5cb6e17c8549a13b05d6698c3f3
liimonx <limonkhan669@gmail.com>
2025-06-15 03:17:50 +0600

    feat: add CSS module type definitions
    - Add TypeScript declarations for CSS modules
    - Improve type safety for CSS imports

---
+++ css.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for CSS imports
+declare module '@shohojdhara/atomix/css' {
+  const content: void;
+  export default content;
+}
+
+declare module '@shohojdhara/atomix/css/min' {
+  const content: void;
+  export default content;
+}
