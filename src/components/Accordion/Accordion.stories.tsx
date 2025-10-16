570f974c5f2c693e92f4b95acb66390b1c40971c
liimonx <limonkhan669@gmail.com>
2025-10-16 09:51:09 +0600

    feat(Accordion): update background image for GlassModeStandard story

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -471,7 +471,7 @@ export const GlassModeStandard: Story = {
   render: args => (
     <div
       style={{
-        background: 'url(https://images.unsplash.com/photo-1518837695005-2083093ee35b?w=1920)',
+        background: 'url(https://images.unsplash.com/photo-1689960159745-fbc6434d4434?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1335)',
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         padding: '3rem',
