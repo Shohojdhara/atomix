ea76f1ed56c336ea314dc21be9c7fc98c9dbc3aa
liimonx <limonkhan669@gmail.com>
2026-05-08 15:25:39 +0600

    chore: clean up project structure and remove unused build/lint artifacts

--- fix_index.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('src/lib/theme/index.ts', 'utf8');
-
-const updated = content.replace(/export\s+\{([^}]+)\}\s+from\s+('[^']+');/g, (match, vars, path) => {
-  return `import {${vars}} from ${path};\nexport {${vars}};`;
-});
-
-fs.writeFileSync('src/lib/theme/index.ts.new', updated);
